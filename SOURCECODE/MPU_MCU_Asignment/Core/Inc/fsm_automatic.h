9267dfb92302b0dd65f9aefbeb36567fc1d90f50
cbct <clongnguynvn@gmail.com>
2023-11-14 20:03:33 +0700

    init MPU_MCU_Assignment

---
+++ SOURCECODE/MPU_MCU_Asignment/Core/Inc/fsm_automatic.h
@@ -0,0 +1,18 @@
+/*
+ * fsm_automatic.h
+ *
+ *  Created on: 14 Nov 2023
+ *      Author: clong
+ */
+
+#ifndef INC_FSM_AUTOMATIC_H_
+#define INC_FSM_AUTOMATIC_H_
+
+extern int T_RED;
+extern int T_AMBER;
+extern int T_GREEN;
+
+void fsm_automatic();
+void led_config();
+
+#endif /* INC_FSM_AUTOMATIC_H_ */
---
+++ SOURCECODE/MPU_MCU_Asignment/Core/Inc/fsm_manual.h
@@ -0,0 +1,15 @@
+/*
+ * fsm_manual.h
+ *
+ *  Created on: 14 Nov 2023
+ *      Author: clong
+ */
+
+#ifndef INC_FSM_MANUAL_H_
+#define INC_FSM_MANUAL_H_
+
+void fsm_red_manual();
+void fsm_amber_manual();
+void fsm_green_manual();
+
+#endif /* INC_FSM_MANUAL_H_ */
